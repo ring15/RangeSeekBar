8fd30365e0a8aa735959d70159188fab53c03264
孙瑞环 <sunruihuan@founq.com>
2019-08-21 11:58:20 +0800

    自定义seekbar，分为普通seekbar和带有刻度的seekbar，拖动图标可以为图片，也可以设置为具体颜色，各种尺寸可设置

--- app/src/main/java/com/founq/sdk/rangeseekbar/MainActivity.java
+++ app/src/main/java/com/founq/sdk/rangeseekbar/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.founq.sdk.rangeseekbar.R;
+import com.founq.sdk.rangeseekbar.widget.RangeBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RangeBar mRangeBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRangeBar = findViewById(R.id.range_bar);
     }
 }
